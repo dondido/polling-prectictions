545060d4cdf7116ef9885fed0998c1111273d46a
dian <dian_t_d@yahoo.co.uk>
2017-04-02 10:56:32 -0700

    Added tags page

--- app/routes.js
+++ app/routes.js
@@ -25,17 +25,26 @@ module.exports = function (app, Poll) {
         const saveCb = error => {
             console.log("Your poll has been saved!");
             const compile = (err, docs) => {
-                const render = (err, content) => {
+                const savePage = (err, content, page) => {
                     console.log(123, content)
-                    fs.writeFile(folder + '/html/compiled/home.html', content, err => {
+                    fs.writeFile(`${folder}/html/compiled/${page}.html`, content, err => {
                         if(err) {
                             return console.log(err);
                         }
                         console.log("The file was saved!");
                     });
                 };
-                console.log(122, docs)
-                es6Renderer(folder + '/html/home.html', {locals: {docs}, template: true}, render);
+                const saveHome = (err, content) => savePage(err, content, 'home');
+                const saveTags = (err, content) => savePage(err, content, 'tags');
+                const tags = {};
+                const extractTags = (total, doc) => total.concat(doc.tags);
+                const countTags = i => tags[i] = tags[i] + 1 || 1;
+                docs.reduce(extractTags, []).forEach(countTags);
+                const pageCount = Math.ceil(docs.length / 10);
+                const pagination = Array(pageCount).fill({attr: 'rel="next"'});
+                pagination[0].attr = '';
+                es6Renderer(folder + '/html/home.html', {locals: {docs, pagination}}, saveHome);
+                es6Renderer(folder + '/html/tags.html', {locals: {tags}}, saveTags);
             }
             if (error) {
                 console.error(error);
@@ -49,7 +58,6 @@ module.exports = function (app, Poll) {
         const setDesc = desc => ({desc});
         rb.options = rb.options.map(setDesc);
         const poll = new Poll(rb);
-        console.log(111, rb);
         poll.save(saveCb);
     });
 
@@ -60,30 +68,42 @@ module.exports = function (app, Poll) {
         },
         partials: {main: folder + '/html/' + (url || req.url) + '.html'}
     });
-    const baseRender = (req, res) => res.render('index', getBaseParams(req));
+    const baseRender = (req, res) => { console.log(getBaseParams(req), 222); res.render('index', getBaseParams(req)) };
     const pollRender = (req, res) => {
         const sendPage = doc => {
-            console.log(111, req.isAuthenticated())
-            const dict = getBaseParams(req, 'question');
-            dict.locals.doc = doc;
-            res.render('index', dict);
+            const checkVotes = option => option.votes.includes(req.sessionID);
+            const voted = doc.options.some(checkVotes);
+            const dict = {
+                locals: {doc},
+            };
+            const renderPage = (err, content) => {
+                dict.locals.questionContent = content;
+                dict.partials = {main: folder + '/html/question.html'};
+                res.render('index', dict);
+            };
+            dict.locals.allVotes = doc.options.reduce((t, o) => t + o.votes.length, 0);
+            if(voted) {
+                return es6Renderer(folder + '/html/question-results.html', dict, renderPage);
+            }
+            dict.locals.token = req.csrfToken();
+            es6Renderer(folder + '/html/question-form.html', dict, renderPage);
         }
         return Poll.findOne({url: req.params.poll}).exec().then(sendPage);
     };
     const vote = (req, res) => {
-        if(req.isAuthenticated()) {
-            const registerAnswer = doc => {
-                const checkVotes = option => option.votes.includes(req.user.email);
-                const voted = doc.options.some(checkVotes);
-                if (voted) {
-                    return redirectHash(req, res, '#voted');
-                }
-                doc.options[req.body.answer].votes.push(req.user.email);
-                doc.save();
-                return redirectHash(req, res, '#show-votes');
-            };
-            Poll.findOne({url: req.params.poll}).exec().then(registerAnswer);
-        }
+        const registerAnswer = doc => {
+            console.log(req.sessionID)
+            const checkVotes = option => option.votes.includes(req.sessionID);
+            const voted = doc.options.some(checkVotes);
+            if (voted) {
+                return redirectHash(req, res, 'back');
+            }
+            doc.options[req.body.answer].votes.push(req.sessionID);
+            doc.save();
+            return redirectHash(req, res, 'back');
+        };
+        Poll.findOne({url: req.params.poll}).exec().then(registerAnswer);
+        
         /*const query = Poll.find({'options.votes': { "$in" : [req.user.email]}});
         return query.exec().then(doc => console.log(123, doc));*/
     };
@@ -98,5 +118,12 @@ module.exports = function (app, Poll) {
     });
     app.get(baseRoutes, baseRender);
     app.get('/polls/:poll', pollRender);
-    app.post('/polls/:poll', vote)
+    app.post('/polls/:poll', vote);
+    app.get('/tags', (req, res) => res.render('index', {partials: {main: folder + '/html/compiled/tags.html'}}));
+    app.get('/tags/:tag', (req, res) => {
+        const renderPage = docs => {
+            console.log(222, docs);
+            res.render('index', {locals: {docs}, partials: {main: folder + '/html/home.html'}})};
+        Poll.find({tags: { $in : [req.params.tag]}}).exec().then(renderPage);
+    });
 };
